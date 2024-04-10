741ff49c82e6ee4443a9c41040ec6bd915e7db0d
Abhi Ram Reddy <127978414+abiredi@users.noreply.github.com>
2024-04-10 16:55:15 -0500

    Update transactions.h
    updating transactions.h

--- transactions.h
+++ transactions.h
@@ -1,21 +1,36 @@
 #ifndef TRANSACTIONS_H
 #define TRANSACTIONS_H
 
-typedef enum{
-    CREATE, DEPOSIT, WITHDRAW,
-    INQUIRY, TRANSFER, CLOSE
-} transType;
+// typedef enum{
+//     CREATE, DEPOSIT, WITHDRAW,
+//     INQUIRY, TRANSFER, CLOSE
+// } transType;
+
+// typedef struct {
+//     transType transactionType;
+//     char accountNumber[20];
+//     double amount;
+//     char timestamp[20];
+//     char recipientAccountNumber[20];
+//     struct Transaction* next;
+// } Transaction;
+
+
+// void appendTransactionToFile(Transaction* transaction);
+
+// #endif
+
+typedef enum {
+    WITHDRAW, CREATE, DEPOSIT, INQUIRY, TRANSFER, CLOSE
+} TransType;
 
 typedef struct {
-    transType transactionType;
+    TransType type;
     char accountNumber[20];
     double amount;
-    char timestamp[20];
     char recipientAccountNumber[20];
-    struct Transaction* next;
 } Transaction;
 
-
-void appendTransactionToFile(Transaction* transaction);
+void* process_transaction(void* arg);
 
 #endif
